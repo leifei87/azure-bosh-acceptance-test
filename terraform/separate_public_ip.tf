2be10fcb3573d05a9c129176843c6751d7c3e9ec
andylei <378513086@qq.com>
2018-12-07 16:19:55 +0800

    Change public ip sku to standard

--- terraform/separate_public_ip.tf
+++ terraform/separate_public_ip.tf
@@ -3,4 +3,5 @@ resource "azurerm_public_ip" "separate" {
   location                     = "${var.location}"
   resource_group_name          = "${azurerm_resource_group.public_ip_resource_group.name}"
   public_ip_address_allocation = "static"
+  sku                          = "Standard"
 }
